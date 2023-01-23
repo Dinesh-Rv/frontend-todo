108afa963e6e650ee98942bb3edbae470458a514
Dinesh-Rv <dineshkumar.ravikumar@ideas2it.com>
2023-01-23 19:01:31 +0530

    completed task hover

--- ApiConnection.js
+++ ApiConnection.js
@@ -1,5 +1,20 @@
 const DEFAULT_URL = "http://localhost:8080/todo/";
 
+/**
+ * Works as a connectivity between data transfer from the backend
+ * and will function multiple api operation at the same time,
+ * it can do both post and get operation depending upon the api method
+ * it gets called with.
+ * @param method - contains the method to apply such as POST and GET,
+ *                 http url will be applied based upon this variable
+ * @param type - type of object the user needs to get or insert
+ *               if the type is category, it does the api operation for category
+ * @param object - contains object that user wants to apply to the api body,
+ *                 works commonly for corresponding post operation
+ * @returns response from the api, post operation returns the true statement or a
+ *          statement regarding the state of post operation, get operation returns the
+ *          object based upon the user mentioned type to get
+ */
 export async function apiConnection(method, type, object) {
   if (method == "POST") {
     let requestObject = getRequestObject(method, object);
@@ -20,6 +35,16 @@ export async function apiConnection(method, type, object) {
   }
 }
 
+/**
+ * For making a request object for body in api operation,
+ * commonly used for post or put operation
+ * converts the js object to json string object
+ * for applying for body in api operation
+ * @param method - contains the api method to apply to request object
+ * @param object - which will be applied to the body for the api operation
+ * @returns the request object itself, which will be used to apply
+ *          for api operation to process the corresponding operation
+ */
 function getRequestObject(method, object) {
   return {
     method: method,
--- backend/src/main/java/com/frontend/todoList/model/Task.java
+++ backend/src/main/java/com/frontend/todoList/model/Task.java
@@ -5,6 +5,19 @@ import lombok.*;
 
 import java.util.List;
 
+/**
+ * Defines a POJO for Tasks of a to-do list,
+ * Gets and sets values for each entity available in task
+ * Contains required constructor for applying a new task
+ * A task contains many attribute such as completed status, list of category ids and
+ * created at to make use of the date origin the task was created
+ * task contain list of category ids to render the required the task to
+ * their corresponding selected category.
+ * id creation of task will be maintainer controller while adding a new task.
+ * Task contains note and status of the note saved which can be used to make the user
+ * know when the note was saved and updated.
+ *
+ */
 @NoArgsConstructor
 @Setter
 @Getter
